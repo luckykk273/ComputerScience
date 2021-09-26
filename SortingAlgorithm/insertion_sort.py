2370f6826d37d9ee721ee68266bc2e0042a4dcb3
luckykk273 <luckykk273@gmail.com>
2021-09-26 19:51:33 +0800

    Rename the variable in the insertion function

--- SortingAlgorithm/insertion_sort.py
+++ SortingAlgorithm/insertion_sort.py
@@ -3,14 +3,14 @@ from SortingAlgorithm.utils import create_array
 
 def insertion_sort(arr):
     for i in range(1, len(arr)):
-        pivot = arr[i]
+        boundary = arr[i]
 
         j = i-1
-        while j > -1 and arr[j] > pivot:
+        while j > -1 and arr[j] > boundary:
             arr[j+1] = arr[j]
             j -= 1
 
-        arr[j+1] = pivot
+        arr[j+1] = boundary
 
 
 if __name__ == '__main__':
